97d44282f64a3bf4ff49771ec45c0647df14131e
EvgeniiS <arbidelsin@gmail.com>
2022-10-26 19:55:23 +0200

    Add a few constructors for insurance plans

--- src/Coding_Exercise_6/Coding_Exercise_7/BronzePlan.java
+++ src/Coding_Exercise_6/Coding_Exercise_7/BronzePlan.java
@@ -1,4 +1,9 @@
 package Coding_Exercise_6.Coding_Exercise_7;
 
 public class BronzePlan extends HealthInsurancePlan {
+
+    public BronzePlan ( double coverage, double discount){
+        setCoverage(coverage / 100);
+        setDiscount(discount);
+    }
 }
--- src/Coding_Exercise_6/Coding_Exercise_7/GoldPlan.java
+++ src/Coding_Exercise_6/Coding_Exercise_7/GoldPlan.java
@@ -1,4 +1,9 @@
 package Coding_Exercise_6.Coding_Exercise_7;
 
 public class GoldPlan extends HealthInsurancePlan {
+
+    public GoldPlan ( double coverage, double discount){
+        setCoverage(coverage / 100);
+        setDiscount(discount);
+    }
 }
