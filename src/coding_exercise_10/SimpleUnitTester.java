415da8549c6b87fcba6d64fc04a8cd4e8beb02ba
EvgeniiS <arbidelsin@gmail.com>
2022-11-08 03:57:23 +0100

    a few edits

--- src/coding_exercise_10/SimpleUnitTester.java
+++ src/coding_exercise_10/SimpleUnitTester.java
@@ -6,13 +6,13 @@ public class SimpleUnitTester {
     public static void main(String[] args) throws Exception {
        Class ReflectionClass = Reflection.class;
 
-        System.out.println("There are " + execute(ReflectionClass) + " methods, began with 'test'");
+        System.out.println("There are " + execute(ReflectionClass) + " methods, began with 'test' and return boolean value");
     }
     public static int execute(Class clazz) throws Exception {
         int failedCount = 0;
        Method[] methodsArray = clazz.getMethods();
        for(Method method : methodsArray){
-           if(method.getName().startsWith("test")){
+           if(method.getName().startsWith("test") && method.getReturnType() == boolean.class){
                failedCount++;
            }
        }
