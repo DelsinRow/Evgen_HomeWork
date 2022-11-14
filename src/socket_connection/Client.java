0ee3f6f45cf710bbe4875c049c4af94bb8e3b448
EvgeniiS <arbidelsin@gmail.com>
2022-11-14 16:47:28 +0100

    add simple chat

--- src/socket_connection/Client.java
+++ src/socket_connection/Client.java
@@ -1,10 +1,31 @@
 package socket_connection;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        Socket clientSocket = new Socket("localhost",4999);
+        Socket clientSocket = new Socket("localhost", 4999);
+        String clientEnter = null;
+        String exitWord = "exit";
+
+        PrintWriter pr = new PrintWriter(clientSocket.getOutputStream());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("client: ");
+        clientEnter = reader.readLine();
+
+        pr.println(clientEnter);
+        pr.flush();
+
+
+        InputStreamReader in = new InputStreamReader(clientSocket.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println("server: " + str);
+
     }
 }
--- src/socket_connection/Server.java
+++ src/socket_connection/Server.java
@@ -1,15 +1,34 @@
 package socket_connection;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(4999);
-        System.out.println("Server running. Wait a client");
-
+        System.out.println("Status: " + ANSI_GREEN + "Server is running..." + ANSI_RESET);
+        System.out.println("Status: " + ANSI_GREEN + "Waiting a client" + ANSI_RESET);
         Socket s = serverSocket.accept();
-        System.out.println("Client connected");
+
+        System.out.println("Status: " + ANSI_GREEN + "Client connected" + ANSI_RESET);
+
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println("client: " + str);
+
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println("Good job ;)");
+        pr.flush();
+
+        System.out.println("Status: " + ANSI_GREEN + "Server was disconnected" + ANSI_RESET);
     }
 }
