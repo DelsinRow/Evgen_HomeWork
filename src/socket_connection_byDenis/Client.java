0b02290a062ac9d48dd2d941ff47d526332fcc56
EvgeniiS <arbidelsin@gmail.com>
2022-11-16 11:56:35 +0100

    ClientServer byDenis

---
+++ src/socket_connection_byDenis/Client.java
@@ -0,0 +1,60 @@
+package socket_connection_byDenis;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Client {
+    // initialize socket
+    private Socket socket = null;
+    // initialize input stream
+    private BufferedReader reader = null;
+    // initialize output stream
+    private DataOutputStream outputStream = null;
+
+    // constructor takes address and port as parameters
+    public Client(String address, int port) {
+        try {
+            // establish a connection
+            socket = new Socket(address, port);
+            System.out.println("MyClientClass#constructor()>>>>>>> Client has been connected");
+
+            // for input purposes (from terminal)
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            // for output purposes (send to the socket)
+            outputStream = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+
+        // string to persist data from input
+        String string = "";
+        System.out.println("MyClientClass#constructor()>>>>>>> Enter messages:");
+
+        while (!string.equals("finish")) {
+            try {
+                string = reader.readLine();
+                outputStream.writeUTF(string);
+            } catch (IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        // close the resources
+        try {
+            socket.close();
+            outputStream.close();
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+
+        System.out.println("MyClientClass#constructor()>>>>>>> Client has been disconnected");
+    }
+
+    public static void main(String args[]) {
+        Client client = new Client("localhost", 9876);
+    }
+}
+
---
+++ src/socket_connection_byDenis/Server.java
@@ -0,0 +1,57 @@
+package socket_connection_byDenis;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    // initialize client socket
+    private Socket clientSocket = null;
+    // initialize server socket
+    private ServerSocket serverSocket = null;
+    // initialize input stream
+    private DataInputStream inputStream = null;
+
+    // constructor takes port as parameter
+    public Server(int port) {
+        try {
+            // Starting server
+            serverSocket = new ServerSocket(port);
+            System.out.println("MyServerClass#constructor()>>>>>>> Server has been started");
+
+            System.out.println("MyServerClass#constructor()>>>>>>> Waiting for a client ...");
+            clientSocket = serverSocket.accept();
+            System.out.println("MyServerClass#constructor()>>>>>>> Client has been joined");
+
+            // for input purposes (from the client)
+            inputStream = new DataInputStream(clientSocket.getInputStream());
+
+            String string = "";
+
+            // until Finish string has been received from the client
+            while (!string.equals("finish")) {
+                try {
+                    string = inputStream.readUTF();
+                    System.out.println("MyServerClass#constructor()>>>>>>> " + string + " message has been received from the client");
+
+                } catch (IOException ex) {
+                    System.out.println(ex);
+                }
+            }
+            System.out.println("\"MyServerClass#constructor()>>>>>>> Closing connection");
+
+            // close the resources
+            clientSocket.close();
+            serverSocket.close();
+            inputStream.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) {
+        Server server = new Server(9876);
+    }
+}
+
